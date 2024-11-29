c3c9d22f71c5b2c14289a25dc132993dc066bce0
kokuuuuun <dracot16@gmail.com>
2024-11-28 22:18:03 -0400

    create the page with the Html, tailwindcss and js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
